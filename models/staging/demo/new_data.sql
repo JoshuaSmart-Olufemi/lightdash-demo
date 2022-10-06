c91b963c14bd056d86ec0629fbd7af175022226a
joshuasmartolufemi@gmail.com <joshuasmartolufemi@gmail.com>
2022-10-06 18:55:37 +0000

    a

--- models/staging/demo/new_data.sql
+++ models/staging/demo/new_data.sql
@@ -3,7 +3,7 @@ with cte as (
     select 
     "order_date" :: date 
     , "due_date" :: date
-    , "order_status" :: integer 
+    , "order_status" :: integer
     , "sales_channel" :: text
     , "customer_id" :: integer 
     , "order_quantity" :: integer
